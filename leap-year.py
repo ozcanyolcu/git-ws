19eab71614bba0a49128090aa0bc403b46aa3019
Sebastian Müller <osmanoeztuerk@icloud.com>
2022-07-13 19:18:06 +0200

    ilk modifiye

--- leap-year.py
+++ leap-year.py
@@ -9,11 +9,9 @@ def CheckLeap(Year):
   # Else it is not a leap year  
   else:  
     print ("Given Year is not a leap Year Cooper")  
-# Taking an input year from user  
-Year = int(input("Enter the Thomas number: "))  
-# Printing result  
-
-CheckLeap(Year) 
+#sdsadsa Printing result  
+dsadsa
+CheckLdsadsadsadsaeap(Year) 
 
 # Celsius u Fahrenat'e çevirme.
 a = int(input('celcius değer girin :'))  # Celsius u Fahrenat'e çevirme.
